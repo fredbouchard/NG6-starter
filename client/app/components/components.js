2c38537d357d664021b11d15fbda3f95ff43c1e3
Frederic Bouchard <fbouchard@landr.com>
2017-09-03 14:58:44 -0400

    refactor: move contacts-list-item for scalability

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,12 +1,10 @@
 import angular from 'angular';
 import ContactsList from './contacts-list';
-import ContactListItem from './contact-list-item';
 import Main from './main';
 
 let componentModule = angular.module('app.components', [
   Main,
-  ContactsList,
-  ContactListItem
+  ContactsList
 ])
 
 .name;
--- client/app/components/contacts-list/index.js
+++ client/app/components/contacts-list/index.js
@@ -1,9 +1,12 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import contactsListItem from './contact-list-item';
 import contactsListComponent from './contacts-list.component';
 
+
 let contactsListModule = angular.module('contactsList', [
-  uiRouter
+  uiRouter,
+  contactsListItem
 ])
   .component('contactsList', contactsListComponent)
   .config(require('./contacts-list.config'))
